d23086d86fd4befd0c49470db68db34093e554e1
Jatin Wig <wigjatin2@gmail.com>
2026-04-08 17:54:32 +0530

    added netlify.toml

--- artifacts/jalsathi/src/App.tsx
+++ artifacts/jalsathi/src/App.tsx
@@ -1,6 +1,6 @@
 import { Switch, Route, Router as WouterRouter } from "wouter";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { ToastProvider } from "./components/ui/Toast";
+import { ToastProvider } from "./components/ui/toast";
 import { IssueProvider } from "./context/IssueContext";
 
 // Components
